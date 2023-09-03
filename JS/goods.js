ec19ffe3701889aee9ad4a9bf35386cd994e6d2d
Андрей Пинтель <110303566+Pintelek@users.noreply.github.com>
2023-09-03 23:02:46 +0300

    Update goods.js

--- JS/goods.js
+++ JS/goods.js
@@ -5,7 +5,7 @@ const goods = {
   "p92779": {
       "name": "Мужские часы CASIO G-2900F-8VER",
       "url": "#",
-      "image": "/images/casio-g-2900f-8ver_images_1650372917.jpg",
+      "image": "./images/casio-g-2900f-8ver_images_1650372917.jpg",
       "price": 1720.00
   },
   "p93039": {
@@ -17,25 +17,25 @@ const goods = {
   "p63553250": {
       "name": "Наручные часы Casio W-800H-1AVES",
       "url": "#",
-      "image": "../images/63553250_images_9154502355.jpg",
+      "image": "./images/63553250_images_9154502355.jpg",
       "price": 484.00
   },
   "p93127": {
       "name": "Мужские часы CASIO EF-552-1AVEF",
       "url": "#",
-      "image": "../images/casio-ef-552-1avef_images_1583730891.jpg",
+      "image": "./images/casio-ef-552-1avef_images_1583730891.jpg",
       "price": 2880.00
   },
   "p79946990": {
       "name": "Мужские часы Casio EF-527D-1AVEF",
       "url": "#",
-      "image": "../images/79946990_images_11571324122.jpg",
+      "image": "./images/79946990_images_11571324122.jpg",
       "price": 4238.00
   },
   "p6533206": {
       "name": "Мужские часы CASIO SGW-100-2BER",
       "url": "#",
-      "image": "../images/6533206_images_1657626044.jpg",
+      "image": "./images/6533206_images_1657626044.jpg",
       "price": 2416.00
   },
 }
